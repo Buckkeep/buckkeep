26db83486923b2d50b599cd41e082c3fef43ec05
Buckkeep <neeta.b+github@gmail.com>
2024-08-29 22:34:38 +0100

    Contact section styled and minor fixes to intro

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,66 @@
 import React from "react";
-import github from '/public/assets/github.png';
+import github from "/public/assets/github.png";
+import linkedin from "/public/assets/linkedin.png";
+import gmail from "/public/assets/google.png";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Contact() {
   return (
     <>
-      <h2>Contact</h2>
-      <img src={github} alt="GitHub" />
+      <section id="contact" className="bg-light my-5 ">
+        <div className="container-lg">
+          <h2>Contact Me</h2>
+          <p className="lead text-muted">Get in touch!</p>
+        </div>
+
+        <Container className="justify-content-center align-items-center">
+          <Row>
+            <Col className="col-4">
+              <Card className="bg-secondary justify-content-center align-items-center">
+                <Card.Img
+                  variant="top"
+                  src={github}
+                  style={{ maxWidth: "10rem" }}
+                  className="p-3"
+                />
+                <Card.Body>
+                  <Button variant="primary" href="https://github.com/Buckkeep">GitHub</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="col-4">
+              <Card  className="bg-secondary justify-content-center align-items-center">
+                <Card.Img
+                  variant="top"
+                  src={linkedin}
+                  style={{ maxWidth: "10rem" }}
+                  className="p-3"
+                />
+                <Card.Body>
+                  <Button variant="primary" href="https://www.linkedin.com/in/neeta-buhecha-2461a41a/">LinkedIn</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="col-4">
+              <Card  className="bg-secondary justify-content-center align-items-center">
+                <Card.Img
+                  variant="top"
+                  src={gmail}
+                  style={{ maxWidth: "10rem" }}
+                  className="p-3"
+                />
+                <Card.Body>
+                  <Button variant="primary" disabled>Gmail</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
   );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Intro() {
   return (
     <>
       <section id="intro">
-        <div className="container-lg">
+        <Container>
           <div className="row justify-content-center align-items-center">
             <div className="col-md-5 text-center text-md-start">
               <h1>
                 <div className="display-1">Black Belt</div>
                 <div className="display-5 text-muted">Your Coding Skills</div>
               </h1>
-              <p class="lead my-4 text-muted">
+              <p className="lead my-4 text-muted">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Laboriosam, dignissimos?
               </p>
-              <a href="#pricing" class="btn btn-secondary btn-lg">
+              <a href="#pricing" className="btn btn-secondary btn-lg">
                 Buy Now
               </a>
             </div>
@@ -23,7 +26,7 @@ function Intro() {
               <img className="img-fluid" src="assets/github.png" alt="GitHub Logo"/>
             </div>
           </div>
-        </div>
+        </Container>
       </section>
     </>
   );
