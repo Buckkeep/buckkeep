a5378aa89c813c4328c8a17ad7e027ed9cf1bee2
Buckkeep <neeta.b+github@gmail.com>
2024-08-30 00:13:07 +0100

    Added a starter carousel to add books and items I like

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Container from "react-bootstrap/esm/Container";
+import bgred from "/public/assets/bgred.png";
+import HTML from "/public/assets/html.png";
+import reactlogo from "/public/assets/react.png";
 
 function About() {
   return (
     <>
-    <h2 className="display-1">Things I Like</h2>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam porro fugit nihil suscipit vel laborum quidem, at nostrum facere est dolore corrupti iste ex, numquam deserunt repudiandae pariatur repellat omnis. Mollitia, doloribus quam veritatis blanditiis quasi repellat harum porro et eum ab! Corporis vel recusandae fugit et cumque repudiandae dicta tenetur eos aliquam! Vel eius in, ullam nam ratione dignissimos. Ea fuga, porro, quisquam eius autem vel culpa doloremque repellat dignissimos possimus ipsum quidem eaque, pariatur amet magni laudantium cupiditate quaerat corrupti tempore tempora. Vero quae asperiores ducimus eveniet explicabo? Id, molestias enim doloribus aliquid laborum praesentium alias optio, perspiciatis ducimus debitis assumenda repudiandae blanditiis tempora odio rem beatae quo nihil, qui labore? Alias neque dolorem quae, voluptatum, nihil, minus velit nesciunt nulla ut eos debitis fugit mollitia quibusdam. Aliquam, quis nemo. Ab sapiente repellat non minus illo odio beatae obcaecati, quasi unde distinctio totam odit quos corporis fuga? Harum error repellat repudiandae iste reiciendis vitae minima iusto perspiciatis dolor accusantium, laudantium repellendus aut, molestiae, debitis officia perferendis in ipsa tempore sed dolorem quas? Asperiores ullam explicabo consectetur dolore quidem distinctio nam quaerat corporis provident! Est laudantium cupiditate tempore quae veniam eaque similique molestiae. Ad, dolorum architecto quas odit delectus quisquam adipisci est nam placeat eveniet doloremque nihil omnis pariatur ullam ipsa repellendus voluptatum iusto ex nemo totam atque, aspernatur animi! Non sit, aperiam et ducimus doloribus natus similique sint repellendus a odit beatae fugit nobis. Mollitia labore quos rerum cupiditate dignissimos iste possimus quas quia nesciunt nemo laboriosam laudantium sint, ratione error omnis voluptas itaque incidunt maxime est recusandae tenetur corporis iusto quae. Impedit molestiae cum molestias ipsum similique quibusdam autem delectus cupiditate qui possimus odit, quis dolores distinctio ducimus laudantium! Laboriosam, eum adipisci laudantium nesciunt cumque nobis quia, cupiditate quasi sequi nemo, at mollitia optio doloremque tempora a.</p>
+      <section id="about">
+        <Container className="my-3">
+          <h2 className="display-5">Things I Like</h2>
+          <Carousel>
+            <Carousel.Item>
+                <img src={bgred} style={{maxHeight: "50rem"}} alt="Javascript" text="First slide" />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={bgred} style={{maxHeight: "50rem"}} alt="Javascript" text="Second slide" />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={bgred} style={{maxHeight: "50rem"}}  alt="Javascript" text="Third slide" />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Container>
+      </section>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
