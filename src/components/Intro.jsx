34c35c48ebf9d21ad22d3b6110953c6469bdd638
Buckkeep <neeta.b+github@gmail.com>
2024-08-29 22:43:27 +0100

    Changed all references to intro to home

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function Intro() {
   return (
     <>
-      <section id="intro">
+      <section id="home" className="my-5">
         <Container>
           <div className="row justify-content-center align-items-center">
-            <div className="col-md-5 text-center text-md-start">
+            <div className="col-md-6 text-center text-md-start">
               <h1>
-                <div className="display-1">Black Belt</div>
-                <div className="display-5 text-muted">Your Coding Skills</div>
+                <div className="display-1">@Buckkeep</div>
+                <div className="display-5 text-muted">Coding is my thing</div>
               </h1>
               <p className="lead my-4 text-muted">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laboriosam, dignissimos?
+                London. Languages. Literature.
               </p>
-              <a href="#pricing" className="btn btn-secondary btn-lg">
-                Buy Now
+              <a href="#contact" className="btn btn-secondary btn-lg">
+                Get in Touch
               </a>
             </div>
             <div className="col-md-5 text-center bg-secondary d-none d-md-block">
-              <img className="img-fluid" src="assets/github.png" alt="GitHub Logo"/>
+              <img className="img-fluid" src="assets/icon.png" alt="Castle Icon"/>
             </div>
           </div>
         </Container>
--- src/components/NavSection.jsx
+++ src/components/NavSection.jsx
@@ -6,11 +6,11 @@ function NavSection() {
   return (
     <Navbar expand="md">
       <Container >
-        <Navbar.Brand href="#intro">Buckkeep's World</Navbar.Brand>
+        <Navbar.Brand href="#home">Buckkeep's World</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#intro">Home</Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#about">Things I like</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
