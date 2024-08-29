7080b1f8c573e7d5647fd6c4e9a6376c25542e56
Buckkeep <neeta.b+github@gmail.com>
2024-08-29 23:50:05 +0100

    Added space for projects. Needs more content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from "react";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Container from 'react-bootstrap/Container';
 
 function Projects() {
   return (
     <>
-    <h2>Projects</h2>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam porro fugit nihil suscipit vel laborum quidem, at nostrum facere est dolore corrupti iste ex, numquam deserunt repudiandae pariatur repellat omnis. Mollitia, doloribus quam veritatis blanditiis quasi repellat harum porro et eum ab! Corporis vel recusandae fugit et cumque repudiandae dicta tenetur eos aliquam! Vel eius in, ullam nam ratione dignissimos. Ea fuga, porro, quisquam eius autem vel culpa doloremque repellat dignissimos possimus ipsum quidem eaque, pariatur amet magni laudantium cupiditate quaerat corrupti tempore tempora. Vero quae asperiores ducimus eveniet explicabo? Id, molestias enim doloribus aliquid laborum praesentium alias optio, perspiciatis ducimus debitis assumenda repudiandae blanditiis tempora odio rem beatae quo nihil, qui labore? Alias neque dolorem quae, voluptatum, nihil, minus velit nesciunt nulla ut eos debitis fugit mollitia quibusdam. Aliquam, quis nemo. Ab sapiente repellat non minus illo odio beatae obcaecati, quasi unde distinctio totam odit quos corporis fuga? Harum error repellat repudiandae iste reiciendis vitae minima iusto perspiciatis dolor accusantium, laudantium repellendus aut, molestiae, debitis officia perferendis in ipsa tempore sed dolorem quas? Asperiores ullam explicabo consectetur dolore quidem distinctio nam quaerat corporis provident! Est laudantium cupiditate tempore quae veniam eaque similique molestiae. Ad, dolorum architecto quas odit delectus quisquam adipisci est nam placeat eveniet doloremque nihil omnis pariatur ullam ipsa repellendus voluptatum iusto ex nemo totam atque, aspernatur animi! Non sit, aperiam et ducimus doloribus natus similique sint repellendus a odit beatae fugit nobis. Mollitia labore quos rerum cupiditate dignissimos iste possimus quas quia nesciunt nemo laboriosam laudantium sint, ratione error omnis voluptas itaque incidunt maxime est recusandae tenetur corporis iusto quae. Impedit molestiae cum molestias ipsum similique quibusdam autem delectus cupiditate qui possimus odit, quis dolores distinctio ducimus laudantium! Laboriosam, eum adipisci laudantium nesciunt cumque nobis quia, cupiditate quasi sequi nemo, at mollitia optio doloremque tempora a.</p>
+      <section id="projects">
+        <Container className="my-3">
+        <h2 className="display-5">Projects</h2>
+        <Tabs
+          defaultActiveKey="profile"
+          id="uncontrolled-tab-example"
+          className="mb-3"
+          justify
+        >
+          <Tab eventKey="project1" title="Project One">
+            Project one coming soon...
+          </Tab>
+          <Tab eventKey="project2" title="Project Two">
+            Project Two coming soon...
+          </Tab>
+          <Tab eventKey="project3" title="Project Three">
+            Project Three coming soon...
+          </Tab>
+        </Tabs>
+        </Container>
+      </section>
     </>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
