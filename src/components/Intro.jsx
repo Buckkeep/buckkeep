895dae2cc8930d1981b1f6817c2d3426206179ab
Buckkeep <neeta.b+github@gmail.com>
2024-08-29 21:23:07 +0100

    Playing around with intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,42 +3,28 @@ import React from "react";
 function Intro() {
   return (
     <>
-      <h2>About</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam porro
-        fugit nihil suscipit vel laborum quidem, at nostrum facere est dolore
-        corrupti iste ex, numquam deserunt repudiandae pariatur repellat omnis.
-        Mollitia, doloribus quam veritatis blanditiis quasi repellat harum porro
-        et eum ab! Corporis vel recusandae fugit et cumque repudiandae dicta
-        tenetur eos aliquam! Vel eius in, ullam nam ratione dignissimos. Ea
-        fuga, porro, quisquam eius autem vel culpa doloremque repellat
-        dignissimos possimus ipsum quidem eaque, pariatur amet magni laudantium
-        cupiditate quaerat corrupti tempore tempora. Vero quae asperiores
-        ducimus eveniet explicabo? Id, molestias enim doloribus aliquid laborum
-        praesentium alias optio, perspiciatis ducimus debitis assumenda
-        repudiandae blanditiis tempora odio rem beatae quo nihil, qui labore?
-        Alias neque dolorem quae, voluptatum, nihil, minus velit nesciunt nulla
-        ut eos debitis fugit mollitia quibusdam. Aliquam, quis nemo. Ab sapiente
-        repellat non minus illo odio beatae obcaecati, quasi unde distinctio
-        totam odit quos corporis fuga? Harum error repellat repudiandae iste
-        reiciendis vitae minima iusto perspiciatis dolor accusantium, laudantium
-        repellendus aut, molestiae, debitis officia perferendis in ipsa tempore
-        sed dolorem quas? Asperiores ullam explicabo consectetur dolore quidem
-        distinctio nam quaerat corporis provident! Est laudantium cupiditate
-        tempore quae veniam eaque similique molestiae. Ad, dolorum architecto
-        quas odit delectus quisquam adipisci est nam placeat eveniet doloremque
-        nihil omnis pariatur ullam ipsa repellendus voluptatum iusto ex nemo
-        totam atque, aspernatur animi! Non sit, aperiam et ducimus doloribus
-        natus similique sint repellendus a odit beatae fugit nobis. Mollitia
-        labore quos rerum cupiditate dignissimos iste possimus quas quia
-        nesciunt nemo laboriosam laudantium sint, ratione error omnis voluptas
-        itaque incidunt maxime est recusandae tenetur corporis iusto quae.
-        Impedit molestiae cum molestias ipsum similique quibusdam autem delectus
-        cupiditate qui possimus odit, quis dolores distinctio ducimus
-        laudantium! Laboriosam, eum adipisci laudantium nesciunt cumque nobis
-        quia, cupiditate quasi sequi nemo, at mollitia optio doloremque tempora
-        a.
-      </p>
+      <section id="intro">
+        <div className="container-lg">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-md-5 text-center text-md-start">
+              <h1>
+                <div className="display-1">Black Belt</div>
+                <div className="display-5 text-muted">Your Coding Skills</div>
+              </h1>
+              <p class="lead my-4 text-muted">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam, dignissimos?
+              </p>
+              <a href="#pricing" class="btn btn-secondary btn-lg">
+                Buy Now
+              </a>
+            </div>
+            <div className="col-md-5 text-center bg-secondary d-none d-md-block">
+              <img className="img-fluid" src="assets/github.png" alt="GitHub Logo"/>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
