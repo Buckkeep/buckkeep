672b6968f0018f43ec0582dab6bda5dd16611b5c
Buckkeep <neeta.b+github@gmail.com>
2024-08-29 21:47:06 +0100

    Adjusted navbar css to expand at medium size

--- src/components/NavSection.jsx
+++ src/components/NavSection.jsx
@@ -4,16 +4,16 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function NavSection() {
   return (
-    <Navbar expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Buckkeep's World</Navbar.Brand>
+    <Navbar expand="md">
+      <Container >
+        <Navbar.Brand href="#intro">Buckkeep's World</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
-            <Nav.Link href="#home">Things I like</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
+            <Nav.Link href="#intro">Home</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#about">Things I like</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
